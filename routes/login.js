23b4d247c76a50ac86325b89fa548df33ed7384a
rfigge24 <rubenfigge24@gmail.com>
2024-03-27 15:00:42 +0100

    login updated

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require("bcrypt")
 
 router.get('/', function(req, res, next) {
   res.render('login');
 });
 
-router.post('/', function(req, res, next) {
-    // handle the authentication
+router.post('/', function(req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    let hash = await bcrypt.hash(password, 13)
+    console.log(hash);
+    res.render('index',{ title: username + hash});
 });
 
 module.exports = router;
