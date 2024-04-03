dafdec8c6ad368d9eff1251a3127642b95dc45c8
jsluijter02 <126278247+jsluijter02@users.noreply.github.com>
2024-04-03 15:31:57 +0200

    books and user database init

--- objects/user.js
+++ objects/user.js
@@ -1,5 +1,5 @@
 // This file is used to describe the structure of the user object, so a user must have an id, username, password etc. It also contains some useful functions for the user database.
-
+const sqlite3 = require("sqlite3").verbose();
 class user{
     // attributes
 
@@ -13,5 +13,27 @@ class user{
         this.address = address;
     }
     // log to db
+
+    addToDB(){
+        let db = new sqlite3.Database("database/books.db", sqlite3.OPEN_READWRITE, (err) => {
+            if (err){
+                console.error(err.message);
+            }
+            console.log("successful connection to books database");
+        });
+
+        let userInfo = [this.id, this.name, this.email, this.username, this.password, this.address]
+        let sqlStatement = "INSERT INTO users(ID, NAME, EMAIL, USERNAME, PASSWORD, ADDRESS) VALUES (?,?,?,?,?,?)";
+        db.run(sqlStatement, userInfo, (err) => {
+            if(err){
+                console.error(err.message);
+            }
+        })
+    }
     // reservation logic?
+
+    // fetch user object from database function -> userobject, if no such user exists, return none/null
+    
 }
+
+module.exports = user;
--- scripts/booksInit.js
+++ scripts/booksInit.js
@@ -1,7 +1,7 @@
 // This file is used to initialize a books/user database
-
 const sqlite3 = require("sqlite3").verbose();
 const book = require("../objects/book"); 
+const user = require("../objects/user");
 
 // create the book/user database
 var db = new sqlite3.Database("database/books.db", (err) => {
@@ -20,6 +20,14 @@ db.run(tableStatement, (err) => {
 });
 console.log("table created")
 
+// create a table for the users
+tableStatement = "CREATE TABLE users (ID, NAME, EMAIL, USERNAME, PASSWORD, ADDRESS)";
+db.run(tableStatement, (err) => {
+    if (err){
+        console.error(err.message);
+    }
+});
+
 // add all starting books to database
 booksArray = [
     new book("test","test","test","test","test",1)
@@ -28,5 +36,19 @@ booksArray = [
 for (bookie of booksArray){
     bookie.addToDB();
 }
+
+// add test user to db
+testUser = new user(1,"john", "test@yeehaw.com", "johnatron", "password", "1234 negro arroya lane NM");
+testUser.addToDB();
+
+function selectRows() {
+    db.each('SELECT username FROM users', (error, row) => {
+      if (error) {
+        throw new Error(error.message);
+      }
+      console.log(row);
+    });
+  };
+selectRows(); 
 
 db.close();
